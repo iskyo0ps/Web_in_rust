de27279ce06076cf53439db4b638f8a8f308654f
iskyo0ps <tsuanxide@gmail.com>
2022-05-23 23:28:03 +0800

    fail to resolve :use of undeclared crate or module

--- ws/webservice/src/handlers.rs
+++ ws/webservice/src/handlers.rs
@@ -6,6 +6,7 @@ pub async fn health_check_handler(
     app_state:web::Data<AppState>
   ) -> HttpResponse{
     let health_check_response = &app_state::health_check_response;
+
     let mut visit_count = app_state::visit_count.lock().unwrap();
     let response =
       format!("{} {} times", health_check_response,visit_count);
